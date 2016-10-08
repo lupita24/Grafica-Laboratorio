aab01d04d378ddefe371e463a4de577a2258b3ab
lupita24 <aniluaf1415@gmail.com>
2016-10-08 01:07:46 -0500

    Update laboratorio.cpp

--- laboratorio.cpp
+++ laboratorio.cpp
@@ -5,29 +5,24 @@
 #include <iostream>
 #include <stdio.h>
 
+#define WIDTH 600
+#define HEIGHT 600
+
+
+int opcion ;
 
 using namespace std;
-#define WIDTH 500
-#define HEIGHT 500
-int n_puntos;
 
 void init()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(-WIDTH/2, WIDTH, -HEIGHT/2, HEIGHT);
-}
-void reshape(int WIDTH, int HEIGHT)
-{
-	glViewport(1, 1, width, height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-2, 2, -2, 2, -2, 2);//proyeccion ortogonal
-	glMatrixMode(GL_MODELVIEW);
+	gluOrtho2D(-WIDTH/2, WIDTH/2, -HEIGHT/2, HEIGHT/2);
 }
 
-void polygons(int puntitos)
+/************DIBUJO DE POLIGONO***************/
+void polygons(int puntos)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1,1,1);
@@ -35,10 +30,11 @@ void polygons(int puntitos)
 	float angulo;
 	vector<float> n_x;
 	vector<float> n_y;
-	angulo=4*asin(1)/puntitos;
+	angulo=4*asin(1)/puntos;
 	float theta=0;
-	for(int i=0 ; i<= puntitos ;i+=1, theta+=angulo)
+	for(int i=0 ; i<= puntos ;i+=1, theta+=angulo)
 	{
+		cout<<i<<" "<<theta<<endl;
 		n_x.push_back(1.0*cos(theta));
 		n_y.push_back(1.0*sin(theta));
 	}
@@ -46,23 +42,26 @@ void polygons(int puntitos)
 	glBegin(GL_LINES);
 	for(int i=1 ; i< n_x.size();i++)
 	{
+		glVertex3f(n_x[i-1],n_y[i-1],0);
 		glVertex3f(n_x[i],n_y[i],0);
 	}	
 	glEnd();
 	glFlush();
 }
 
-void lines_with_points(int x0, int x1, int y0, int y1)
+/*************BIDUJO DE LIINEA CON PUNTOS - METODO PUNTO MEDIO***************/
+void line_with_point(int x0, int x1, int y0, int y1)
 {
+	
 	int dx = x1 - x0, dy = y1 - y0;
     int iterax = 1, iteray = 1;
 
-    if(dx < 0)//Si el x1 es menor que x0
+    if(dx < 0) //Si el x1 es menor que x0
     {
       dx = -dx; iterax = -1;
-  	}
+    }
 
-    if(dy < 0)//Si el y1 es menor que y0
+    if(dy < 0) //Si el y1 es menor que y0
     {
       dy = -dy; iteray = -1;
     }
@@ -107,10 +106,14 @@ void lines_with_points(int x0, int x1, int y0, int y1)
       }
     }
     glEnd();
+
 }
 
 
-void drawPuntosImagen(int cx, int cy, int x, int y){
+/****************DIBUJO DE CIRCULO**********************/
+
+
+void draw_image_point(int cx, int cy, int x, int y){
     glBegin(GL_POINTS);
         glVertex2i(cx + x, cy + y);
         glVertex2i(cx + y, cy + x);
@@ -126,7 +129,7 @@ void drawPuntosImagen(int cx, int cy, int x, int y){
 
 void circle(int cx, int cy, int radio){
     int x = 0, y = radio, d = 1 - radio;
-    drawPuntosImagen(cx, cy, x, y);
+    draw_image_point(cx, cy, x, y);
     while(y > x){
         if(d < 0){
             d = d + 2*x + 3;
@@ -135,95 +138,151 @@ void circle(int cx, int cy, int radio){
             d = d + 2*(x - y) + 5;
             x = x + 1;
             y = y - 1;}
-        drawPuntosImagen(cx, cy, x, y);
+        draw_image_point(cx, cy, x, y);
     }
 }
 
-void drawCircle(int radio){
+void draw_circle(int radio){
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(0.0, 0.0, 1.0);
     circle(0,0, radio);
 }
 
-void ellipse_point(const int& x, const int& y, const int&cx, const int& cy)
-{
-	
-	int difx = abs(cx-x);
-	int dify = abs(cy-y);
+
+/**********************DIBUJO DE ELIPSE****************************/
+
+
+void ellipse_points(const int& x, const int& y, const int&cx, const int& cy){
 		
-	glBegin(GL_POINTS);
-	glVertex2i( cx + difx, cy + dify);//glVertex2i( x, y);
-	glVertex2i( cx + difx, cy-dify);
-	glVertex2i(cx -difx, cy+dify);
-	glVertex2i(cx-difx, cy-dify);
-	glEnd();
+		int difx = abs(cx-x);
+		int dify = abs(cy-y);
 		
-}
+		glBegin(GL_POINTS);
+		glVertex2i( cx + difx, cy + dify);//glVertex2i( x, y);
+		glVertex2i( cx + difx, cy-dify);
+		glVertex2i(cx -difx, cy+dify);
+		glVertex2i(cx-difx, cy-dify);
+		glEnd();
+		
+	}
 	
 void draw_ellipse(const int& cx, const int& cy, const int& a, const int& b)
 {
 	int x,  y;
 	float d1, d2;
-	/* valores iniciales  */
+	/* Valores iniciales */
 	x = 0;
-	y = b;	
+	y = b;
 	d1 = b * b - a * a * b + a * a / 4.0;
-	ellipse_point(x+cx, y+cy,cx,cy); /*Simetria de orden 4 */
+	ellipse_points(x+cx, y+cy,cx,cy); /* Simetria de orden 4 */
 	while(a * a * (y - 0.5) > b * b * (x + 1))
 	{
 		/* Region 1 */
 		if (d1 < 0)
 		{
 			d1 = d1 + b * b * (2 * x + 3);
-			x++; 
+			x++;
 		}
-		  else
+		else
 		{
 			d1 = d1 + b * b * (2 * x + 3) + a * a * (-2 * y + 2);
 			x++;
 			y--;
 		}
+		ellipse_points(x+cx, y+cy,cx,cy);
 	}
+
+		d2 = b * b * (x + 0.5) * (x + 0.5) + a * a * (y - 1) * (y - 1) - a * a * b * b;
 	
-	d2 = b * b * (x + 0.5) * (x + 0.5) + a * a * (y - 1) * (y - 1) - a * a * b * b;
-	
-	while(y > 0)
-	{
+	while(y > 0){
 		/* Region 2 */
 		if (d2 < 0)
 		{
 			d2 = d2 + b * b * (2 * x + 2) + a * a * (-2 * y + 3);
 			x++;
-			y--;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+			y--;
 		}
 		else
 		{
 			d2 = d2 + a * a * (-2 * y + 3);
 			y--;
 		}
-		ellipse_point(x+cx, y+cy,cx,cy);
+		ellipse_points(x+cx, y+cy,cx,cy);
 	}
 }
 
+/********************DIBUJO DE HIPERBOLA**************************/
+void hyperbole(int escala)
+{
+	float y,x;
+	float a1=12;
+	float b1 = 9;
+	glColor3f (1, 1, 0);
 
-void myDisplay() {
-	draw_ellipse(10,40,40,10);
-	//polygons(4);
+	glBegin (GL_POINTS);
+	for ( x = -(escala); x <escala; x++)     // Horizontal axis hyperbola
+	{  
+		y = a1 * sqrt (1 + (x*x)/(b1*b1));	
+		glVertex2f (x,  y);
+		glVertex2f (x, -y);
+	}
+	glEnd ();
+
+	glColor3f (1, 0.4, 0.4);                    // Vertical axis hyperbola
+
+	glBegin (GL_POINTS);
+	for ( y = -(escala); y < escala; y++)  
+	{     
+		x = a1 * sqrt (1 + (y*y)/(b1*b1));
+		glVertex2f ( x, y);
+		glVertex2f (-x, y);
+	}
+	glEnd();
 	glFlush();
-	glutReshapeFunc(reshape);
-	
+}
+
+void myDisplay() 
+{
+	 switch ( opcion )
+      {
+         case 1:
+            polygons(6);
+            glFlush();
+            break;
+         case 2:
+            line_with_point(90,10,30,100);
+            glFlush();
+            break;
+         case 3:
+            draw_circle(250);
+           	glFlush(); 
+            break;
+         case 4:
+            draw_ellipse(50,50,80,50);
+           	glFlush(); 
+            break;
+         case 5:
+            hyperbole(100);
+           	glFlush();
+            break;
+      }	
 }
 
 int main(int argc, char **argv)
 {
-	printf( "Ingresa el numero de puntos\n");
-	scanf("%d", &n_puntos);
+	printf( "Ingresa la opcion que desee\n");
+	printf( "1 - Dibujo de poligono\n");
+	printf( "2 - Linea con puntos\n");
+	printf( "3 - Dibujo de circulo\n");
+	printf( "4 - Dibujo de elipse\n");
+	printf( "5 - Dibujo de hiperbola\n");
+	scanf("%d", &opcion);
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
 	glutInitWindowSize(WIDTH, HEIGHT);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Guadalupe's graphics");
+	glutCreateWindow("Opengl's graphics");
 	init();
 	glutDisplayFunc(myDisplay);
 	glutMainLoop();
